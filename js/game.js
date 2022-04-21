d50866111b87159e452a805372a71ef6227a9928
debajyoti-byjus <debajyoti.mmajumder@byjus.com>
2022-04-21 11:28:11 +0530

    Level 1 build process

--- js/game.js
+++ js/game.js
@@ -8,8 +8,13 @@ gameScene.preload = function () {
     this.load.image('lightON1', '../assets/light ON1.png');
     this.load.image('lightON2', '../assets/light ON2.png');
     this.load.image('lightON3', '../assets/light ON3.png');
+    this.load.image('cell', '../assets/battery.png');
+
     this.load.image('bg', '../assets/circuit2.png');
+    this.load.image('dot', '../assets/Test dot for locating the bulb holder spots .png');
+
 }
+
 //called after preload ends
 gameScene.create = function () {
     let screenheight = window.innerHeight;
@@ -18,58 +23,133 @@ gameScene.create = function () {
     let topAlignment = 0.25 * screenheight;
     // Create bg sprite
     // Pic Aspect ratio - 1200/800
-    let bg = this.add.sprite(0, 0, 'bg').setScale(0.4 * screenwidth / 1200);
+    let bgWidth = 929, bgHeight = 385;
+    let scaleFactor = 0.45 * screenwidth / bgWidth;
+    let scaleFactor2 = scaleFactor * 0.35;
+    let distThreshhold = 150;
+    let bg = this.add.sprite(0, 0, 'bg').setScale(scaleFactor);
     // Phaser.Display.Align.In.Center(bg, this.add.zone(400, 300, 800, 600));    //  Center the picture in the game
-    // bg.setOrigin(0, 0); //change the  origin of the asset to top-left corner
-    bg.setPosition(screenwidth / 2.5, screenheight / 2); //place sprite postiion in the center
+    bg.setOrigin(0, 0); //change the  origin of the asset to top-left corner
+    bg.setPosition(screenwidth * 0.2, screenheight * 0.2); //place sprite postiion in the center
 
 
-    var image0 = this.add.sprite(leftAlignment, topAlignment * 1, 'lightOFF').setScale(0.3 * screenwidth / 1200).setInteractive();
-    // console.log(image0);
-    // image0.scale.setTo(0.5, 0.5);
+    function landingSpotFinder(a, b) {
+        //first find the location of the corner of the circuit image
+        let shiftx = screenwidth * 0.2;
+        let shifty = screenheight * 0.2;
+        //find the centre of the image(keep scaling in mind)
+
+        shiftx += a * scaleFactor * bgWidth;
+        shifty += b * scaleFactor * bgHeight;
+        return {
+            xCoordinate: shiftx,
+            yCoordinate: shifty,
+        };
+        //example of use of above function
+        // let value = landingSpotFinder(a, b);
+        // let x = value.xCoordinate;
+        // let y = value.yCoordinate;
+    }
+    let leftPercent = [0.25, 0.5, 0.75];  //here, 0.517= 51.7%
+    let topPercent = [.99, .99, .99];  //here, 0.05= 5%
+    // let testDot = this.add.sprite(0, 0, 'dot');// TEST DOT for finding the position of the bulb holder locations in the circut
+    // let DotXcoordinateValue = landingSpotFinder(1, 1).xCoordinate;
+    // let DotYcoordinateValue = landingSpotFinder(1, 1).yCoordinate;
+    // testDot.setPosition(DotXcoordinateValue, DotYcoordinateValue);
+
+    //Fixed bulb for level 1
+    let fixedBulb = this.add.sprite(0, 0, 'lightON1').setScale(scaleFactor2);// TEST DOT for finding the position of the bulb holder locations in the circut
+    let fixedBulbTop = -0.05;
+    let fixedBulbleft = 0.49;
+    let fixedBulbXcoordinateValue = landingSpotFinder(fixedBulbleft, fixedBulbTop).xCoordinate;
+    let fixedBulbYcoordinateValue = landingSpotFinder(fixedBulbleft, fixedBulbTop).yCoordinate;
+    fixedBulb.setPosition(fixedBulbXcoordinateValue, fixedBulbYcoordinateValue);
+
+    //Cell 1
+    var image0 = this.add.sprite(leftAlignment, topAlignment * 1, 'cell').setScale(scaleFactor2).setInteractive();
+    image0.name = "1st_cell"; // unique identifier for this game object( gameObject.name will give access to it)
     this.input.setDraggable(image0);
-    //  The pointer has to move 16 pixels before it's considered as a drag
-    // this.input.dragDistanceThreshold = 0;
 
-    let bulbPositionX, bulbPositionY;
-    this.input.on('dragstart', function (pointer, gameObject) {
-        // save the initial position of the bulbs4
-        bulbPositionX = gameObject.x;
-        bulbPositionY = gameObject.y;
+    let cellInitPos = [
+        [leftAlignment, topAlignment * 1],
+        [leftAlignment, topAlignment * 2],
+        [leftAlignment, topAlignment * 3]
+    ]; //state variable stores the intial position of the bateries (x and y coordinate)
+
+    let cellTargetPos = [
+        [landingSpotFinder(leftPercent[0], topPercent[0]).xCoordinate, landingSpotFinder(leftPercent[0], topPercent[0]).yCoordinate],
+        [landingSpotFinder(leftPercent[1], topPercent[1]).xCoordinate, landingSpotFinder(leftPercent[1], topPercent[1]).yCoordinate],
+        [landingSpotFinder(leftPercent[2], topPercent[2]).xCoordinate, landingSpotFinder(leftPercent[2], topPercent[2]).yCoordinate]
+    ]; //state variable stores the target position of the bateries (x and y coordinate)
+
+    let cellCurrentPos = [
+        [leftAlignment, topAlignment * 1],
+        [leftAlignment, topAlignment * 2],
+        [leftAlignment, topAlignment * 3]
+    ]; //state variable stores the current position of the bateries (x and y coordinate)
+
+    let iscellSnapped = [false, false, false]; //state variable stores if a particular cell is snapped to a location.
+
+    function spriteNumberFinder(gameObject) {
+        let index;
+        if (gameObject.name == "1st_cell") index = 0;
+        if (gameObject.name == "2nd_cell") index = 1;
+        if (gameObject.name == "3rd_cell") index = 2;
+        return index;
+    }
+    function nearOrNot(x1, y1, x2, y2, distThreshhold) {
+        /* This function takes two coordinates and tells if they are close or not.
+        If they are near, it returns TRUE else FALSE */
+        if (Math.abs(x1 - x2) <= distThreshhold * 2 && Math.abs(y2 - y1) <= distThreshhold) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    // this.input.on('dragstart', function (pointer, gameObject, dragX, dragY) {
+    //     console.log("started dragging");
+    // });
+    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+        let index = spriteNumberFinder(gameObject);
+        console.log(cellCurrentPos[index][0]);
+        console.log(cellCurrentPos[index][1]);
+
+        cellCurrentPos[index][0] = dragX;
+        cellCurrentPos[index][1] = dragY;
+        gameObject.x = dragX;
+        gameObject.y = dragY;
+        if (nearOrNot(cellCurrentPos[index][0], cellCurrentPos[index][1], cellTargetPos[index][0], cellTargetPos[index][1], distThreshhold)) {
+            console.log("Target reached");
+        }
     });
     this.input.on('dragend', function (pointer, gameObject) {
-        // gameObject.setTint(0xff0000);
-        gameObject.x = bulbPositionX;
-        gameObject.y = bulbPositionY;
+        let index = spriteNumberFinder(gameObject);
+        //if cell is near a holder location then place the cell in that location.
+        if (nearOrNot(cellCurrentPos[index][0], cellCurrentPos[index][1], cellTargetPos[index][0], cellTargetPos[index][1], distThreshhold)) {
+            gameObject.x = cellTargetPos[index][0];
+            gameObject.y = cellTargetPos[index][1];
+        }
+        else {
+            gameObject.x = cellInitPos[index][0];
+            gameObject.y = cellInitPos[index][1];
+        }
+        // console.log(nearOrNot(cellCurrentPos[index][0], cellCurrentPos[index][1], cellTargetPos[index][0], cellTargetPos[index][1], 30));
     });
 
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-    });
-    // this.input.on('dragend', function (pointer, gameObject) {
-    //     // gameObject.clearTint();
-    // });
-
-
-    // Light ON 1
-    var image1 = this.add.sprite(leftAlignment, topAlignment * 2, 'lightOFF').setScale(0.3 * screenwidth / 1200).setInteractive();
+    // cell 2
+    var image1 = this.add.sprite(leftAlignment, topAlignment * 2, 'cell').setScale(scaleFactor2).setInteractive();
+    image1.name = "2nd_cell";
     this.input.setDraggable(image1);
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-    });
 
-    // Light ON 2
-    var image2 = this.add.sprite(leftAlignment, topAlignment * 3, 'lightOFF').setScale(0.3 * screenwidth / 1200).setInteractive();
+    // cell 3
+    var image2 = this.add.sprite(leftAlignment, topAlignment * 3, 'cell').setScale(scaleFactor2).setInteractive();
+    image2.name = "3rd_cell";
     this.input.setDraggable(image2);
-    this.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-        gameObject.x = dragX;
-        gameObject.y = dragY;
-    });
+    // game.add.tween(image2.scale).to({ x: 2, y: 2 }, 2000, Phaser.Easing.Linear.None, true);
 
 }
-
 
 
 // set the configuration of the scene
@@ -82,13 +162,21 @@ let config = {
 
 //Create a new Game, pass the configuration
 let game = new Phaser.Game(config);
-
-// gameScene.update = function () {
-//     // game.scale.pageAlignHorizontally = true;
-//     // game.scale.pageAlignVertically = true;
-//     game.scale.refresh();
-//     // this.bg.x += 1;
-// }
+let c1 = 0;
+gameScene.update = function () {
+    // game.scale.pageAlignHorizontally = true;
+    // game.scale.pageAlignVertically = true;
+    // game.scale.refresh();
+    // this.bg.x += 1;
+    // Phaser.Actions.RotateAroundDistance([this.container], this.center, this.rotateSpeed, 250);
+    // const angleDeg = Math.atan2(this.container.y - this.center.y, this.container.x - this.center.x) * 180 / Math.PI;
+    // this.container.angle = angleDeg + 90 // container should face the center point
+    // if (c1 == 0) {
+    //     console.log(gameScene.add.displayList.scene.add.displayList.list[4]);
+    //     c1 += 1;
+    // }
+    // Phaser.Actions.ScaleXY(gameScene.gameObject, 0.5);
+}
 
 // let canvas = document.get
 // window.addEventListener("resize", redraw);
